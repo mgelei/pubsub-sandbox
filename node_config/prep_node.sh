d20d18b85a9f62ad19e570fec9e3cdaf0fce40de
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2024-11-21 13:01:09 +0000

    Node config templates

--- node_config/prep_node.sh
+++ node_config/prep_node.sh
@@ -6,22 +6,45 @@
 ##############################################################################################################
 
 # Allow nopasswd sudoing
-echo "%sudo ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/nopasswd
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/nopasswd
+
+# SSH setup
+mkdir -p ~/.ssh
+curl https://github.com/mgelei.keys > ~/.ssh/authorized_keys
+echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config
+
 
 # Update OS
-sudo apt-get update && sudo apt-get upgrade -y
+sudo dnf update -y && sudo reboot
+
+# We're using an external firewall
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo systemctl mask firewalld
 
 # Create config file
 sudo mkdir -p /etc/rancher/rke2/
+sudo touch /etc/rancher/rke2/config.yaml
+sudo chmod o-r /etc/rancher/rke2/config.yaml
 sudo nano /etc/rancher/rke2/config.yaml
 
 ### Install RKE2 as usual, see the node config yaml templates for the config file
+curl -sfL https://get.rke2.io | sh -
+systemctl enable rke2-server.service
+systemctl start rke2-server.service &
+journalctl -u rke2-server -f
+
+### Or the agent:
+curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sh -
+systemctl enable rke2-agent.service
+systemctl start rke2-agent.service &
+journalctl -u rke2-agent -f
 
 # Helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 # Access kubeconfig
 # (Then again, normally you'd want to grab the kubeconfig from the RKE2 server, instead of doing this)
-echo "alias k='sudo /var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml'" >> ~/.bash_aliases
-echo "alias h='sudo helm --kubeconfig /etc/rancher/rke2/rke2.yaml'" >> ~/.bash_aliases
-. ~/.bash_aliases
+echo "alias k='sudo /var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml'" >> ~/.bashrc
+echo "alias h='sudo /usr/local/bin/helm --kubeconfig /etc/rancher/rke2/rke2.yaml'" >> ~/.bashrc
+. ~/.bashrc
