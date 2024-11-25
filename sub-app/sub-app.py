8c7a910be2957eba99d7a5f5792992e28683c114
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2024-11-25 20:04:04 +0100

    Sub app

---
+++ sub-app/sub-app.py
@@ -0,0 +1,17 @@
+import datetime
+import pika
+
+def callback(ch, method, properties, body):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Received {body} at {timestamp}")
+
+if __name__ == '__main__':
+    connection_parameters = pika.ConnectionParameters(
+        host="rabbitmq-cluster.svc.cluster.local", # this shouldn't be hardcoded
+        virtual_host="fph",
+        credentials=pika.PlainCredentials("admin", "admin")) # this most definitely shouldn't be hardcoded - use k8s secrets instead
+    connection = pika.BlockingConnection(connection_parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue='proba-feladat')
+    channel.basic_consume(queue='proba-feladat', on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
