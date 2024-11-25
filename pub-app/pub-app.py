aa54713086039f0f92504718cebe63ec79079a30
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2024-11-25 20:34:08 +0100

    Pub app blocking connection

--- pub-app/pub-app.py
+++ pub-app/pub-app.py
@@ -7,7 +7,7 @@ async def produce_numbers(channel):
     while True:
         random_number = random.randint(1, 5)
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"{timestamp} Generated random number: {random_number}")
+        print(f"{timestamp} Published number: {random_number}")
         channel.basic_publish(exchange='', routing_key='proba-feladat',
                               body=f"{timestamp} {random_number}")
         await asyncio.sleep(5)
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         host="rabbitmq-cluster.svc.cluster.local", # this shouldn't be hardcoded
         virtual_host="fph",
         credentials=pika.PlainCredentials("admin", "admin")) # this most definitely shouldn't be hardcoded - use k8s secrets instead
-    connection = pika.SelectConnection(connection_parameters)
+    connection = pika.BlockingConnection(connection_parameters)
     channel = connection.channel()
     channel.queue_declare(queue='proba-feladat')
 
